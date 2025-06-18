1f886d926240a781e48daf1939f103ae2d9f979b
Damien <DamienB2474@protonmail.com>
2025-06-17 21:50:28 -0400

    Fixed bug with routes

--- community-connectivity-orlando/src/App.jsx
+++ community-connectivity-orlando/src/App.jsx
@@ -7,7 +7,7 @@ import Request from './routes/request/request';
 import Signup from './routes/signup/signup';
 import Data from "./routes/admin/data/data";
 import Manage from "./routes/admin/manage/manage";
-import Profile from './routes/admin/profile/profile';
+import AdminProfile from "./routes/admin/profile/profile";
 import Profiles from './routes/admin/profiles/profiles';
 import Requests from './routes/admin/requests/requests';
 import Panel from "./routes/admin/panel/panel";
@@ -16,23 +16,20 @@ import AdminLogin from "./routes/admin/login/login";
 function App() {
   return (
     <Routes>
-      <Route path="/">
-      <Route index element={<Home/>}/>
-      <Route path="/signup" element={<Signup/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/request" element={<Request/>}/>
-      <Route path="/profile" element={<Profile/>}/>
-      </Route>
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/request" element={<Request />} />
+      <Route path="/profile" element={<Profile />} />
 
       {/* Admin Routes */}
-      <Route path="/admin">
-        <Route index element={<Panel/>}/>
-        <Route path="/data" element={<Data/>}/>
-        <Route path="/manage" element={<Manage/>}/>
-        <Route path="/profile" element={<Profile/>}/>
-        <Route path="/profiles" element={<Requests/>}/>
-        <Route path="/login" element={<AdminLogin/>}/>
-      </Route>
+      <Route path="/admin" element={<Panel />} />
+      <Route path="/admin/data" element={<Data />} />
+      <Route path="/admin/manage" element={<Manage />} />
+      <Route path="/admin/profile" element={<AdminProfile />} />
+      <Route path="/admin/profiles" element={<Profiles />} />
+      <Route path="/admin/requests" element={<Requests />} />
+      <Route path="/admin/login" element={<AdminLogin />} />
     </Routes>
   );
 }
