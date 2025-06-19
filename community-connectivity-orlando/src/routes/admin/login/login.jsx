c4fcf5cd2cdcb67d9e088b63f4e1864fa4e2b884
Damien <1204electro@gmail.com>
2025-06-19 17:06:09 -0400

    added admin & started on signup

--- community-connectivity-orlando/src/routes/admin/login/login.jsx
+++ community-connectivity-orlando/src/routes/admin/login/login.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Button, Card, Form } from "react-bootstrap";
+import "./login.scss";
 
 function Login() {
-    return ( 
-        <div>
-            <h1>Login</h1>
-        </div>
-     );
+  return (
+    <div className="login-wrapper">
+      <Card className="login-card">
+        <Card.Body>
+          <div className="text-center mb-4 color">
+            <Card.Title as="h2">Orlando City Table Rentals</Card.Title>
+            <Card.Subtitle className="mb-2">Admin Log in</Card.Subtitle>
+          </div>
+          <Form>
+            <Form.Group className="mb-3" controlId="formEmail">
+              <Form.Label className="white">Email</Form.Label>
+              <Form.Control required type="email" placeholder="Email" />
+            </Form.Group>
+            <Form.Group className="mb-4" controlId="formPassword">
+              <Form.Label className="white">Password</Form.Label>
+              <Form.Control required type="password" placeholder="Password" />
+            </Form.Group>
+            <Button type="submit" className="w-100 btn-color">
+              <strong>Log in</strong>
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
 
 export default Login;
--- community-connectivity-orlando/src/routes/signup/signup.jsx
+++ community-connectivity-orlando/src/routes/signup/signup.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Button, Card, Form } from "react-bootstrap";
 
 function Signup() {
-    return ( 
-        <div>
-            <h1>Signup page</h1>
-        </div>
-     );
+  return (
+    <div className="signup-wrapper">
+      <Card>
+        <Card.Body>
+          <div className="text-cener mb-2">
+            <Card.Title>Orlando City Table Rentals</Card.Title>
+            <Card.Subtitle className="mb-2">Register</Card.Subtitle>
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
 
 export default Signup;
+
