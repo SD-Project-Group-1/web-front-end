e6d7213e9110dad612050cf91e1dba49e1902e94
Damien <1204electro@gmail.com>
2025-06-23 11:04:19 -0400

    Finished signup, login, admin/login forms

--- community-connectivity-orlando/src/routes/login/login.jsx
+++ community-connectivity-orlando/src/routes/login/login.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import { useState } from "react";
 import { Button, Card, Form } from "react-bootstrap";
 import "./login.scss";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("page submitted");
+    console.log(`Email: ${email} Password: ${password}`);
+  };
+
   return (
     <div className="login-wrapper">
       <Card className="login-card">
@@ -11,20 +20,34 @@ function Login() {
             <Card.Title as="h2">Orlando City Table Rentals</Card.Title>
             <Card.Subtitle className="mb-2">Log in</Card.Subtitle>
           </div>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formEmail">
               <Form.Label className="white">Email</Form.Label>
-              <Form.Control required type="email" placeholder="Email" />
+              <Form.Control
+                required
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </Form.Group>
             <Form.Group className="mb-4" controlId="formPassword">
               <Form.Label className="white">Password</Form.Label>
-              <Form.Control required type="password" placeholder="Password" />
+              <Form.Control
+                required
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </Form.Group>
             <Button type="submit" className="w-100 btn-color">
               <strong>Log in</strong>
             </Button>
           </Form>
-          <a href="/admin/login" className="color">Admin Log in</a>
+          <a href="/admin/login" className="color">
+            Admin Log in
+          </a>
         </Card.Body>
       </Card>
     </div>
