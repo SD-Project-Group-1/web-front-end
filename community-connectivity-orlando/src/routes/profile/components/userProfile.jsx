02b1474f120bcb6169b9bea41f416e12d76e67a6
Neil E. <elkadineil@gmail.com>
2025-07-01 13:55:30 -0400

    Added admin profile

---
+++ community-connectivity-orlando/src/routes/profile/components/userProfile.jsx
@@ -0,0 +1,71 @@
+import { Button } from "react-bootstrap";
+import styles from "../profile.module.scss";
+import { useContext } from "react";
+import { UserContext } from "../../../context/userContext";
+import { useNavigate } from "react-router-dom";
+
+export default function UserProfile({ user }) {
+  const navigate = useNavigate();
+  const { setUser } = useContext(UserContext);
+
+  const logout = async () => {
+    await fetch("/api/signout", { method: "POST" });
+    setUser(null);
+    navigate("/");
+  };
+
+  return (
+    <div className={`${styles.container}`}>
+      <div className={`${styles["user-card"]}`}>
+        <h1>{user.first_name} {user.last_name}</h1>
+        <p>UserID: {user.id}</p>
+        <p>{user.phone}</p>
+        <p>{user.email}</p>
+        <p>Age: {user.dob}</p>
+        <address>
+          {user.street_address},<br />
+          {user.city},<br />
+          {user.zip_code}
+        </address>
+      </div>
+      <div className={`${styles["user-fields"]}`}>
+        <h1>Profile Information</h1>
+        <div className={`${styles["data-fields-container"]}`}>
+          <div className={`${styles["data-fields"]}`}>
+            <div className={`${styles["field"]}`}>
+              <label>First name</label>
+              <input name="firstName" defaultValue={user.first_name} />
+            </div>
+            <div className={`${styles["field"]}`}>
+              <label>Last name</label>
+              <input name="lastName" defaultValue={user.last_name} />
+            </div>
+            <div className={`${styles["field"]}`}>
+              <label>Number</label>
+              <input name="number" type="number" defaultValue={user.phone} />
+            </div>
+            <div className={`${styles["field"]}`}>
+              <label>Email</label>
+              <input name="email" type="email" defaultValue={user.email} />
+            </div>
+            <div className={`${styles["field"]}`}>
+              <label>Date of Birth</label>
+              <input name="dob" type="date" defaultValue={new Date(user.dob)} />
+            </div>
+            <div className={`${styles["field"]}`}>
+              <label>Address</label>
+              <input name="address" defaultValue={user.street_address} />
+            </div>
+          </div>
+          <Button>Update info</Button>
+        </div>
+        <div className={`${styles.actions}`}>
+          <h1>Account Actions</h1>
+          <Button>Reset Password</Button>
+          <Button onClick={logout}>Logout</Button>
+          <Button>Delete Account</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- community-connectivity-orlando/src/routes/profile/profile.jsx
+++ community-connectivity-orlando/src/routes/profile/profile.jsx
@@ -1,19 +1,13 @@
 import { useContext } from "react";
 
-import styles from "./profile.module.scss";
-import { Button } from "react-bootstrap";
 import { UserContext } from "../../context/userContext";
 import { useNavigate } from "react-router-dom";
+import UserProfile from "./components/userProfile";
+import AdminProfile from "./components/adminProfile";
 
 function Profile() {
   const { user, setUser, loading } = useContext(UserContext);
   const navigate = useNavigate();
-
-  const logout = async () => {
-    await fetch("/api/signout", { method: "POST" });
-    setUser(null);
-    navigate("/");
-  };
 
   if (loading) {
     return <h1>Please wait...</h1>;
@@ -24,60 +18,11 @@ function Profile() {
     return;
   }
 
-  return (
-    <div className={`${styles.container}`}>
-      <div className={`${styles["user-card"]}`}>
-        <h1>{user.first_name} {user.last_name}</h1>
-        <p>UserID: {user.id}</p>
-        <p>{user.phone}</p>
-        <p>{user.email}</p>
-        <p>Age: {user.dob}</p>
-        <address>
-          {user.street_address},<br />
-          {user.city},<br />
-          {user.zip_code}
-        </address>
-      </div>
-      <div className={`${styles["user-fields"]}`}>
-        <h1>Profile Information</h1>
-        <div className={`${styles["data-fields-container"]}`}>
-          <div className={`${styles["data-fields"]}`}>
-            <div className={`${styles["field"]}`}>
-              <label>First name</label>
-              <input name="firstName" defaultValue={user.first_name} />
-            </div>
-            <div className={`${styles["field"]}`}>
-              <label>Last name</label>
-              <input name="lastName" defaultValue={user.last_name} />
-            </div>
-            <div className={`${styles["field"]}`}>
-              <label>Number</label>
-              <input name="number" type="number" defaultValue={user.phone} />
-            </div>
-            <div className={`${styles["field"]}`}>
-              <label>Email</label>
-              <input name="email" type="email" defaultValue={user.email} />
-            </div>
-            <div className={`${styles["field"]}`}>
-              <label>Date of Birth</label>
-              <input name="dob" type="date" defaultValue={new Date(user.dob)} />
-            </div>
-            <div className={`${styles["field"]}`}>
-              <label>Address</label>
-              <input name="address" defaultValue={user.street_address} />
-            </div>
-          </div>
-          <Button>Update info</Button>
-        </div>
-        <div className={`${styles.actions}`}>
-          <h1>Account Actions</h1>
-          <Button>Reset Password</Button>
-          <Button onClick={logout}>Logout</Button>
-          <Button>Delete Account</Button>
-        </div>
-      </div>
-    </div>
-  );
+  if (user.role && user.role !== "user") {
+    return <UserProfile user={user} />;
+  } else {
+    return <AdminProfile admin={user} />;
+  }
 }
 
 export default Profile;
