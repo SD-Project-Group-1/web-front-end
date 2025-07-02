ae8e76c4a9ac29db541960581e7de540b8ed5533
Neil E. <elkadineil@gmail.com>
2025-07-02 01:33:33 -0400

    Added routes for viewing other user and admin profiles

--- community-connectivity-orlando/src/routes/profile/profile.jsx
+++ community-connectivity-orlando/src/routes/profile/profile.jsx
@@ -1,13 +1,68 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 
 import { UserContext } from "../../context/userContext";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import UserProfile from "./components/userProfile";
 import AdminProfile from "./components/adminProfile";
+import { useState } from "react";
 
 function Profile() {
+  const { userId, adminId } = useParams();
   const { user, loading } = useContext(UserContext);
+  const [profileOf, setProfileOf] = useState(null);
+
+  console.log(setProfileOf);
+
   const navigate = useNavigate();
+
+  const setToAdminProfile = async (id) => {
+    const response = await fetch(`/api/admin/get/${id}`);
+
+    if (!response.ok) {
+      alert("Could not get that profile!");
+      setInterval(() => navigate("/"), 3000);
+    }
+
+    const data = response.json();
+    setProfileOf(data);
+  };
+
+  const setToUserProfile = async (id) => {
+    const response = await fetch(`/api/user/get/${id}`);
+
+    if (!response.ok) {
+      alert("Could not get that profile!");
+      setInterval(() => navigate("/"), 3000);
+    }
+
+    const data = await response.json();
+
+    setProfileOf(data);
+  };
+
+  useEffect(() => {
+    if (!user) return;
+
+    if (adminId) {
+      if (user?.role !== "management") {
+        navigate("/");
+        return;
+      }
+
+      setToAdminProfile(adminId);
+    } else if (userId) {
+      if (!user?.role || user.role === "user") {
+        navigate(`/`);
+        return;
+      }
+
+      alert("AGGG");
+
+      setToUserProfile(userId);
+    } else {
+      setProfileOf(user);
+    }
+  }, [user]);
 
   if (loading) {
     return <></>;
@@ -18,10 +73,14 @@ function Profile() {
     return;
   }
 
-  if (user?.role && user.role !== "user") {
-    return <AdminProfile admin={user} />;
+  if (!profileOf) {
+    return <></>;
+  }
+
+  if (profileOf?.role && profileOf.role !== "user") {
+    return <AdminProfile admin={profileOf} />;
   } else {
-    return <UserProfile user={user} />;
+    return <UserProfile user={profileOf} />;
   }
 }
 
