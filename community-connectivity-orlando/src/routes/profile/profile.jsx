b55e866dc68b89cbd737e957d54bc8ed242cd89e
Damien <1204electro@gmail.com>
2025-07-05 18:53:50 -0400

    reset files from main

--- community-connectivity-orlando/src/routes/profile/profile.jsx
+++ community-connectivity-orlando/src/routes/profile/profile.jsx
@@ -1,22 +1,69 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 
-import styles from "./profile.module.scss";
-import { Button } from "react-bootstrap";
 import { UserContext } from "../../context/userContext";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import UserProfile from "./components/userProfile";
+import AdminProfile from "./components/adminProfile";
+import { useState } from "react";
 
 function Profile() {
-  const { user, setUser, loading } = useContext(UserContext);
+  const { userId, adminId } = useParams();
+  const { user, loading } = useContext(UserContext);
+  const [profileOf, setProfileOf] = useState(null);
+
+  console.log(setProfileOf);
+
   const navigate = useNavigate();
 
-  const logout = async () => {
-    await fetch("/api/signout", { method: "POST" });
-    setUser(null);
-    navigate("/");
+  const setToAdminProfile = async (id) => {
+    const response = await fetch(`/api/admin/get/${id}`);
+
+    if (!response.ok) {
+      alert("Could not get that profile!");
+      setInterval(() => navigate("/"), 3000);
+    }
+
+    const data = response.json();
+    setProfileOf(data);
   };
 
+  const setToUserProfile = async (id) => {
+    const response = await fetch(`/api/user/get/${id}`);
+
+    if (!response.ok) {
+      alert("Could not get that profile!");
+      setInterval(() => navigate("/"), 3000);
+    }
+
+    const data = await response.json();
+
+    setProfileOf(data);
+  };
+
+  useEffect(() => {
+    if (!user) return;
+
+    if (adminId) {
+      if (user?.role !== "management") {
+        navigate("/");
+        return;
+      }
+
+      setToAdminProfile(adminId);
+    } else if (userId) {
+      if (!user?.role || user.role === "user") {
+        navigate(`/`);
+        return;
+      }
+
+      setToUserProfile(userId);
+    } else {
+      setProfileOf(user);
+    }
+  }, [user]);
+
   if (loading) {
-    return <h1>Please wait...</h1>;
+    return <></>;
   }
 
   if (user === null) {
@@ -24,60 +71,15 @@ function Profile() {
     return;
   }
 
-  return (
-    <div className={`${styles.container}`}>
-      <div className={`${styles["user-card"]}`}>
-        <h1>{user.first_name} {user.last_name}</h1>
-        <p>UserID: {user.id}</p>
-        <p>{user.phone}</p>
-        <p>{user.email}</p>
-        <p>Age: {user.dob}</p>
-        <address>
-          {user.street_address},<br />
-          {user.city},<br />
-          {user.zip_code}
-        </address>
-      </div>
-      <div className={`${styles["user-fields"]}`}>
-        <h1>Profile Information</h1>
-        <div className={`${styles["data-fields-container"]}`}>
-          <div className={`${styles["data-fields"]}`}>
-            <div className={`${styles["field"]}`}>
-              <label>First name</label>
-              <input name="firstName" defaultValue={user.first_name} />
-            </div>
-            <div className={`${styles["field"]}`}>
-              <label>Last name</label>
-              <input name="lastName" defaultValue={user.last_name} />
-            </div>
-            <div className={`${styles["field"]}`}>
-              <label>Number</label>
-              <input name="number" type="number" defaultValue={user.phone} />
-            </div>
-            <div className={`${styles["field"]}`}>
-              <label>Email</label>
-              <input name="email" type="email" defaultValue={user.email} />
-            </div>
-            <div className={`${styles["field"]}`}>
-              <label>Date of Birth</label>
-              <input name="dob" type="date" defaultValue={new Date(user.dob)} />
-            </div>
-            <div className={`${styles["field"]}`}>
-              <label>Address</label>
-              <input name="address" defaultValue={user.street_address} />
-            </div>
-          </div>
-          <Button>Update info</Button>
-        </div>
-        <div className={`${styles.actions}`}>
-          <h1>Account Actions</h1>
-          <Button>Reset Password</Button>
-          <Button onClick={logout}>Logout</Button>
-          <Button>Delete Account</Button>
-        </div>
-      </div>
-    </div>
-  );
+  if (!profileOf) {
+    return <></>;
+  }
+
+  if (profileOf?.role && profileOf.role !== "user") {
+    return <AdminProfile admin={profileOf} />;
+  } else {
+    return <UserProfile user={profileOf} />;
+  }
 }
 
 export default Profile;
