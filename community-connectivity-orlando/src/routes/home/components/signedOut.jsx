bba8f12a5cc9dc3517145224dd6d8202c339a812
Neil E. <elkadineil@gmail.com>
2025-07-18 22:16:21 -0400

    Everything is responsive

--- community-connectivity-orlando/src/routes/home/components/signedOut.jsx
+++ community-connectivity-orlando/src/routes/home/components/signedOut.jsx
@@ -28,13 +28,15 @@ export default function SignedOut() {
         <Button onClick={checkZipCode}>Check</Button>
       </div>
       <h2>Make an account</h2>
-      <div className={`${styles["make-account-box"]}`}>
+      <div className="">
         <p>
           All you need to get started is to make an account and provide some
           basic information.
         </p>
         <Link to={"/signup"}>
-          <Button>Sign Up</Button>
+          <div className="w-100 d-flex justify-content-center">
+            <Button className="fs-1 fw-bold">Sign Up</Button>
+          </div>
         </Link>
       </div>
     </div>
