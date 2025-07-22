83c3a9893c5114031038a560fdfc1530d2b9aeb9
Neil E. <elkadineil@gmail.com>
2025-07-22 11:27:09 -0400

    These were missed

---
+++ community-connectivity-orlando/src/helpers/date.js
@@ -0,0 +1,11 @@
+export const toLocalDateInput = (date) => {
+  const pad = (n) => n.toString().padStart(2, '0');
+
+  const yyyy = date.getFullYear();
+  const MM = pad(date.getMonth() + 1);
+  const dd = pad(date.getDate());
+  const hh = pad(date.getHours());
+  const mm = pad(date.getMinutes());
+
+  return `${yyyy}-${MM}-${dd}T${hh}:${mm}`;
+}
